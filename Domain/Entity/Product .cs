48ddb278553b939cba90ffbbd4b12361801f9da9
DESKTOP-QV3DI8R\Coasia_MI_Laptop <levantung0000@gmail.com>
2024-02-16 14:12:28 +0700

    a

---
+++ Domain/Entity/Product .cs
@@ -0,0 +1,15 @@
+﻿using Domain.IAudittable;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entity
+{
+    public class Product: BaseEntity
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
---
+++ Domain/IAudittable/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.IAudittable
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
